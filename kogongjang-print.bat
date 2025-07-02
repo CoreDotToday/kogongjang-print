a5a22b617a123427311d2e5db701f974a91121c7
jourmooney <79959835+jourmooney@users.noreply.github.com>
2025-07-02 16:28:58 +0900

    Update kogongjang-print.bat

--- kogongjang-print.bat
+++ kogongjang-print.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: 1. 프로젝트 폴더로 확실하게 이동 (중요)
-cd /d "C:\Users\CORE\Downloads\kogongjang-print"
+cd /d "C:\Users\CORE\Documents\kogongjang-print-main"
 
 :: 2. 가상환경의 python.exe를 직접 지정하여 app.py 실행
 ::    이것이 activate를 대체하는 가장 확실한 방법입니다.
